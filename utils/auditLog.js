728c1051e6dcbae0606e62a1097acde403e1832a
pobruno <brunoshy@gmail.com>
2024-12-03 23:18:12 -0300

    slash commands

--- utils/auditLog.js
+++ utils/auditLog.js
@@ -1,12 +1,12 @@
 const { EmbedBuilder } = require('discord.js');
 
-async function sendAuditLog(message) {
+async function sendAuditLog(client, message) {
     const channel = await client.channels.fetch(process.env.LOG_CHANNEL_ID);
     const embed = new EmbedBuilder()
         .setColor('#FF5733')
         .setDescription(message)
         .setTimestamp();
-    
+
     await channel.send({ embeds: [embed] });
 }
 
